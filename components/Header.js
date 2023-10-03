8778669d49a7d23d369db187b516f16ae8c0748b
ShotaKitamura <nordvillaggio.s.0215@gmail.com>
2023-10-04 00:55:52 +0900

    ♻️ メイン部分のコンポーネント化

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,15 @@
+"use client"
+
 import styles from '../src/app/page.module.css'
 
 export function Header(props) {
-    console.info(props);
   return (
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          
-          <code className={styles.code}>src/app/{props.page}.js</code>
+          {props.children}
         </p>
+
       </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Links } from '../../components/Links'
-import { Header } from '../../components/Header'
+"use client"
+
+import { Main } from '../../components/Main'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-
-      <Header page="index" />
-
-      <div className={styles.description}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <Links />
-
-    </main>
+    <Main root="app" page="index"/>
   )
 }
